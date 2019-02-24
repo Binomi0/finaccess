42fd9c4507fdb9041062fb1165289e71c935f8f2
Adolfo Onrubia <adolfo@onrubia.es>
2019-02-24 18:56:19 +0100

    remove unnecesary logs

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -8,7 +8,6 @@ import { StyledCharacter, Content } from '../styles/character';
 
 const Character = ({ selected, handleClick, idx, character }) => {
   const { comics, stories, series, name, description, thumbnail } = character;
-  console.log('Character =>', character);
   return (
     <StyledCharacter onClick={() => handleClick(idx)} selected={selected}>
       <CharacterHeader
